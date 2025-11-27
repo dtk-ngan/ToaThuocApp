3f6987fbd20c41a6386981c0bf7ff90654291d4b
dtk-ngan <dtkngan.bvctch@gmail.com>
2025-11-27 09:06:12 +0700

    Layout Form Tiếp Nhận

---
+++ ToaThuoc/FormTiepNhan.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToaThuoc
+{
+    public partial class FormTiepNhan : Form
+    {
+        public FormTiepNhan()
+        {
+            InitializeComponent();
+        }
+
+        private void FormTiepNhan_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
